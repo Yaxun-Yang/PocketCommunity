5025e2f9b2c676b03b77ae90f18391823daa5233
YaxunYang <yyxunn@outlook.com>
2020-07-31 01:43:34 +0800

    update sql

--- sql/pocketcommunity.sql
+++ sql/pocketcommunity.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 30/07/2020 00:56:47
+ Date: 31/07/2020 01:42:49
 */
 
 SET NAMES utf8mb4;
@@ -24,13 +24,14 @@ DROP TABLE IF EXISTS `activity`;
 CREATE TABLE `activity`  (
   `activityId` int(50) NOT NULL AUTO_INCREMENT,
   `text` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_es_0900_ai_ci NOT NULL,
   PRIMARY KEY (`activityId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of activity
 -- ----------------------------
+INSERT INTO `activity` VALUES (2, '这是一个社区活动', 'activityPicture1.png');
 
 -- ----------------------------
 -- Table structure for material
@@ -38,16 +39,17 @@ CREATE TABLE `activity`  (
 DROP TABLE IF EXISTS `material`;
 CREATE TABLE `material`  (
   `materialId` int(50) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `materialName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `origin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `destination` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `transferStation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   PRIMARY KEY (`materialId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of material
 -- ----------------------------
+INSERT INTO `material` VALUES (1, '物资1', '四川', '花园路21号', '城北1号仓库');
 
 -- ----------------------------
 -- Table structure for message
@@ -57,16 +59,16 @@ CREATE TABLE `message`  (
   `messageId` int(50) NOT NULL AUTO_INCREMENT,
   `userId` int(50) NOT NULL,
   `text` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `reply` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `messageTimestamp` timestamp(0) NOT NULL,
   PRIMARY KEY (`messageId`) USING BTREE,
   INDEX `fk_1`(`userId`) USING BTREE,
   CONSTRAINT `fk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of message
 -- ----------------------------
+INSERT INTO `message` VALUES (1, 1, '这是一个留言', '2020-07-30 17:13:04');
 
 -- ----------------------------
 -- Table structure for notification
@@ -76,25 +78,32 @@ CREATE TABLE `notification`  (
   `notificationId` int(50) NOT NULL AUTO_INCREMENT,
   `text` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`notificationId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of notification
 -- ----------------------------
+INSERT INTO `notification` VALUES (1, '这是一个社区公告');
 
 -- ----------------------------
 -- Table structure for questionnaire
 -- ----------------------------
 DROP TABLE IF EXISTS `questionnaire`;
 CREATE TABLE `questionnaire`  (
-  `questionaireId` int(11) NOT NULL AUTO_INCREMENT,
+  `questionnaireId` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY (`questionaireId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`questionnaireId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of questionnaire
 -- ----------------------------
+INSERT INTO `questionnaire` VALUES (1, 'http://www.example.com');
+INSERT INTO `questionnaire` VALUES (2, 'http://www.example.com');
+INSERT INTO `questionnaire` VALUES (3, 'http://www.example.com');
+INSERT INTO `questionnaire` VALUES (4, 'http://www.example.com');
+INSERT INTO `questionnaire` VALUES (5, 'http://www.example.com');
+INSERT INTO `questionnaire` VALUES (6, 'http://www.example.com');
 
 -- ----------------------------
 -- Table structure for users
@@ -102,7 +111,7 @@ CREATE TABLE `questionnaire`  (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `userId` int(50) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `idCartNum` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -111,10 +120,11 @@ CREATE TABLE `users`  (
   `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
   `admin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`userId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
+INSERT INTO `users` VALUES (1, '张三', 'zhangsan', '01.jpg', '111111111111', '11111111', '玫瑰园', '东门路', 'N');
 
 SET FOREIGN_KEY_CHECKS = 1;
